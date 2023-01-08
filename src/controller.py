2e5c157b444c0f7442bd24cbf68642fcee721946
Fernando Ordoñez <fordezcontacto@gmail.com>
2023-01-08 14:18:29 -0500

    add sender action

--- src/controller.py
+++ src/controller.py
@@ -1,10 +1,11 @@
 from src.dialogflow import sendDialogflow
-from src.reply import responseMessenger
+from src.reply import responseMessenger, senderAction
 
 def controller(payload):
     
     match payload['message_type']:
         case 'text':
+            senderAction(payload['sender_id'])
             ai = sendDialogflow(payload['message'], 12345)
             print(ai)
             responseMessenger(payload['sender_id'], ai['answer'])
--- src/reply.py
+++ src/reply.py
@@ -3,12 +3,13 @@ import json
 from src.setting import API_KEY_FACEBOOK
 
 API_KEY = API_KEY_FACEBOOK
-
-def responseMessenger(sender_id, text):
-    url = f'https://graph.facebook.com/v14.0/me/messages?access_token={API_KEY}'
-    headers = {
+url = f'https://graph.facebook.com/v14.0/me/messages?access_token={API_KEY}'
+headers = {
         "Content-Type": "application/json"
     }
+
+def responseMessenger(sender_id, text):
+
     datos = {
         "messaging_type": "RESPONSE",
         "recipient": {
@@ -21,3 +22,11 @@ def responseMessenger(sender_id, text):
     response = requests.post(url, data=json.dumps(datos), headers=headers)
     print(response.text)
 
+def senderAction(sender_id):
+    datos = {
+            "recipient":{
+            "id":sender_id
+        },
+        "sender_action":"typing_on"
+    }
+    response = requests.post(url, data=json.dumps(datos), headers=headers)
